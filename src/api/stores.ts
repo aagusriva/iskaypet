dde6e3d40244a78c960772e0f1b52603a3bb961c
aagusriva <aagusriva@gmail.com>
2024-02-17 12:36:36 -0300

    adding check in screen and animations

--- src/api/stores.ts
+++ src/api/stores.ts
@@ -2,10 +2,6 @@ import axios from 'axios';
 import {API_IKP_URL} from './routes';
 import {Store} from '../interfaces/store';
 
-/**
- * Return a list of articles with pagination and searching.
- * Return the following props of items: id, title, artist_title, description, image_id
- */
 export const getStores = async (): Promise<Store[] | null> => {
   try {
     const resp = await axios({
@@ -17,4 +13,36 @@ export const getStores = async (): Promise<Store[] | null> => {
     console.log(error);
   }
   return null;
+};
+
+type PostCheckInData = {
+  storeId: string;
+  taskId: string;
+};
+
+export const postCheckin = async (data: PostCheckInData): Promise<boolean> => {
+  try {
+    const resp = await axios({
+      url: `${API_IKP_URL}/checkin`,
+      method: 'POST',
+      data,
+    });
+    return resp.status >= 200 && resp.status < 400;
+  } catch (error) {
+    console.log(error);
+  }
+  return false;
+};
+
+export const resetStore = async (): Promise<boolean> => {
+  try {
+    const resp = await axios({
+      url: `${API_IKP_URL}/stores/reset`,
+      method: 'POST',
+    });
+    return resp.status >= 200 && resp.status < 400;
+  } catch (error) {
+    console.log(error);
+  }
+  return false;
 };
--- src/screens/CheckInScreen.tsx
+++ src/screens/CheckInScreen.tsx
@@ -1,24 +1,119 @@
-import {useRoute} from '@react-navigation/native';
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import LottieView from 'lottie-react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
+import {postCheckin, resetStore} from '../api/stores';
+import {Button} from '@rneui/themed';
+import {useTranslation} from 'react-i18next';
+import {COLORS} from '../constants/Colors';
+
+const LOADING_ANIMATION = require('../../assets/json/loading.json');
+const SUCCESS_ANIMATION = require('../../assets/json/success.json');
+const FAIL_ANIMATION = require('../../assets/json/fail.json');
+
+type PropsAnimation = {
+  loading: boolean;
+  success: boolean;
+};
+
+const RenderAnimation = ({loading, success}: PropsAnimation) => {
+  if (!loading) {
+    return (
+      <LottieView
+        source={success ? SUCCESS_ANIMATION : FAIL_ANIMATION}
+        autoPlay
+        style={styles.lottie}
+        loop={false}
+      />
+    );
+  } else {
+    return (
+      <LottieView
+        source={LOADING_ANIMATION}
+        autoPlay
+        loop
+        style={styles.lottie}
+      />
+    );
+  }
+};
 
 const CheckInScreen = () => {
+  const {t} = useTranslation('');
+  const navigation = useNavigation();
   const route = useRoute();
   const storeId = (route.params as {storeId: string; taskId: string})?.storeId;
   const taskId = (route.params as {storeId: string; taskId: string})?.taskId;
+  const [loading, setLoading] = useState(true);
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    apiCall(storeId, taskId);
+  }, [storeId, taskId]);
+
+  const apiCall = useCallback(async (storeId: string, taskId: string) => {
+    setLoading(true);
+    const resp = await postCheckin({storeId, taskId});
+    // const resp = await resetStore();
+    if (resp) {
+      setSuccess(true);
+    } else {
+      setSuccess(false);
+    }
+    setLoading(false);
+  }, []);
+
+  const goHome = () => {
+    return navigation.navigate(...(['Home'] as never));
+  };
 
   return (
     <View style={styles.container}>
-      <Text>{storeId}</Text>
-      <Text>{taskId}</Text>
+      <RenderAnimation loading={loading} success={success} />
+      {!success && (
+        <Button
+          title={t('checkin.retry')}
+          onPress={() => apiCall(storeId, taskId)}
+          buttonStyle={styles.button}
+          containerStyle={styles.buttonContainer}
+        />
+      )}
+      {!loading && (
+        <Button
+          title={t('checkin.goHome')}
+          type={!success ? 'clear' : 'solid'}
+          onPress={goHome}
+          containerStyle={{
+            ...styles.buttonContainer,
+            bottom: !success ? 50 : 100,
+          }}
+          buttonStyle={success ? styles.button : undefined}
+          titleStyle={!success ? styles.link : undefined}
+        />
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
-    paddingBottom: 70,
+    height: '100%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  lottie: {width: 200, height: 200},
+  buttonContainer: {
+    width: '90%',
+    position: 'absolute',
+    bottom: 100,
+  },
+  button: {
+    width: '100%',
+    backgroundColor: COLORS.primary,
+  },
+  link: {
+    color: COLORS.primary,
   },
 });
 
